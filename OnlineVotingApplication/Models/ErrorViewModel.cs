9de03dea4a87678b2013ea45343eb4f5d241fccd
Matheus E. P. Victor <73787080+maexpe@users.noreply.github.com>
2023-01-28 11:13:25 -0300

    Adicionar arquivos de projeto.

---
+++ OnlineVotingApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineVotingApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
