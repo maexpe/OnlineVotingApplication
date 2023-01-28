a18f406ecc6d0f7585f7ef507f1b703ddf26c1f3
Matheus E. P. Victor <73787080+maexpe@users.noreply.github.com>
2023-01-28 15:45:47 -0300

    feat: Addition of migration.

--- OnlineVotingApplication/Models/DbInitializer.cs
+++ OnlineVotingApplication/Models/DbInitializer.cs
@@ -1,6 +1,46 @@
-﻿namespace OnlineVotingApplication.Models
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineVotingApplication.Areas.Identity.Data;
+using OnlineVotingApplication.Data;
+
+namespace OnlineVotingApplication.Models
 {
-    public class DbInitializer
+    public static class DbInitializer
     {
+        public static async Task InitializeAsync(OnlineVotingApplicationContext context, IServiceProvider serviceProvider, UserManager<OnlineVotingApplicationUser> userManager)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            
+            string[] roleTitles = { "Admin", "Voter", "Candidate" };
+            
+            IdentityResult roleResult;
+            
+            foreach (var RoleTitle in roleTitles)
+            {
+                var roleExists = await RoleManager.RoleExistsAsync(RoleTitle);
+
+                if (!roleExists)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(RoleTitle));
+                }
+            }
+
+            string Email = "admin@maexpe.com";
+            string Password = "9K1CBTr3$*QK";
+
+            if (userManager.FindByEmailAsync(Email).Result == null)
+            {
+                OnlineVotingApplicationUser user = new OnlineVotingApplicationUser();
+
+                user.Username = Email;
+                user.Email = Email;
+
+                IdentityResult result = userManager.CreateAsync(user, Password).Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+        }
     }
 }
