85271e9bede70dde9edc574ed5887a0faf313d7c
Matheus E. P. Victor <73787080+maexpe@users.noreply.github.com>
2023-01-28 13:46:37 -0300

    feat: Addition of OnlineVotingApplicationUser properties.

--- OnlineVotingApplication/Areas/Identity/Data/OnlineVotingApplicationUser.cs
+++ OnlineVotingApplication/Areas/Identity/Data/OnlineVotingApplicationUser.cs
@@ -9,5 +9,22 @@ namespace OnlineVotingApplication.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the OnlineVotingApplicationUser class
 public class OnlineVotingApplicationUser : IdentityUser
 {
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string ProfileImage { get; set; }
+    public string Phone { get; set; }
+    public string MobilePhone { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string AddressComplement { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string Country { get; set; }
+    public bool IsVoter { get; set; }
 }
 
